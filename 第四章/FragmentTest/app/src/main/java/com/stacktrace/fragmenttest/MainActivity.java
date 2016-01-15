df8a18cfe3877b0d158222317f1c913f85f4dd81
stacktrace828 <stacktrace828@gmail.com>
2016-01-15 22:26:12 +0800

    复习完第四章

---
+++ 第四章/FragmentTest/app/src/main/java/com/stacktrace/fragmenttest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.stacktrace.fragmenttest;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnotherRightFragment fragment = new AnotherRightFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_layout, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+    }
+}
